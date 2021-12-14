44225f57fe10624447a94694b092ffa4886702f9
Hamza <hamuft24@students.shrewsbury.k12.ma.us>
2021-12-14 08:44:25 -0500

    feat: fixed img adding - to finish

--- animations.py
+++ animations.py
@@ -1,1 +1,16 @@
-# pictures/animations for each step of the story
+# init turtle + screen
+import turtle as trtl
+screen_h = 400
+screen_w = 420
+
+# init story + character
+wn = trtl.Screen()
+cac_jck = trtl.Turtle
+wn.setup(width=screen_w, height=screen_h)
+cactus_img = ""
+wn.addshape(cactus_img)
+
+# def animations for each choice
+'''def chc_0():
+    
+def chc_1():'''
